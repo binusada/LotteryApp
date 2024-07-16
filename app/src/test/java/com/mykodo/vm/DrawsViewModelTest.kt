7321f1ef587677b546f1a892a69a3d607019a814
binusada <binusadanand@gmail.com>
2024-07-16 17:01:52 +0100

    Add .gitignore to ignore .gradle and .idea directories

---
+++ app/src/test/java/com/mykodo/vm/DrawsViewModelTest.kt
@@ -0,0 +1,26 @@
+package com.mykodo.vm
+
+import org.junit.jupiter.api.Assertions.*
+
+class DrawsViewModelTest {
+
+    @org.junit.jupiter.api.Test
+    fun getUiState() {
+    }
+
+    @org.junit.jupiter.api.Test
+    fun fetchDraws() {
+    }
+
+    @org.junit.jupiter.api.Test
+    fun getCurrencyFormatter() {
+    }
+
+    @org.junit.jupiter.api.Test
+    fun getDateFormatHelper() {
+    }
+
+    @org.junit.jupiter.api.Test
+    fun getNumberFormatter() {
+    }
+}
